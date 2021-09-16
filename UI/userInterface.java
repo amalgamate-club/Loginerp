4559baa66e5805c6c6e460778f26668143ebacd9
Rohan R <sonurohan27@gmail.com>
2021-09-16 21:02:39 +0530

    created UI directory

---
+++ UI/userInterface.java
@@ -0,0 +1,7 @@
+class userInterface
+{
+    public static void main(String[] args) {
+        System.out.println("Here is the code for UI");
+        System.out.println("We will be using Swing GUI");
+    }
+}
